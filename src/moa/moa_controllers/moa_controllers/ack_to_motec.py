f3238a196a9182c72eb349fe3047cb8048470dd4
Adam Keating <adam.keating@fsae.co.nz>
2023-03-02 17:59:12 +1300

    created subscriber that listens to /cmd_val topic for stamped ackermann msg

--- src/moa/moa_controllers/moa_controllers/ack_to_motec.py
+++ src/moa/moa_controllers/moa_controllers/ack_to_motec.py
@@ -2,10 +2,23 @@
 import rclpy
 from rclpy.node import Node
 
+from ackermann_msgs import AckermannDriveStamped
+
 class ack_to_mot(Node):
     def __init__(self):
         super().__init__('ackermann_to_motec') #node name
-        
+
+        #create subscriber
+        self.subscription = self.create_subscription(
+            AckermannDriveStamped,     #msg type
+            'cmd_val',                 #topic receiving from
+            self.listener_callback,    #callback function
+            10                         #qos profile
+        )
+
+    def listener_callback(self):
+        pass
+
 
 def main(args=None):
     rclpy.init(args=args)
