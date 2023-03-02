7498abf63d6f523ac1df9521490ce94420c766e3
Adam Keating <adam.keating@fsae.co.nz>
2023-03-02 18:38:58 +1300

    created diagnostics publisher and added basic framework for CAN srv and heartbeat

--- src/moa/moa_controllers/moa_controllers/ack_to_motec.py
+++ src/moa/moa_controllers/moa_controllers/ack_to_motec.py
@@ -3,18 +3,37 @@ import rclpy
 from rclpy.node import Node
 
 from ackermann_msgs import AckermannDriveStamped
+from diagnostic_msgs import DiagnosticStatus
 
 class ack_to_mot(Node):
     def __init__(self):
         super().__init__('ackermann_to_motec') #node name
 
-        #create subscriber
+        #create subscriber for ackermann input
         self.subscription = self.create_subscription(
             AckermannDriveStamped,     #msg type
             'cmd_val',                 #topic receiving from
             self.listener_callback,    #callback function
             10                         #qos profile
         )
+
+        #create publisher for diagnostics
+        self.diagnostics = self.create_publisher(
+            DiagnosticStatus,          #msg type
+            "diagnostics",             #topic sent to
+            10                         #qos
+        )
+        
+        # #create client for heartbeat server
+        # self.heartbeat = self.create_client(
+            
+        # )
+
+        # #create server for CAN
+        # self.server = self.create_service(
+
+        # )
+
 
     def listener_callback(self):
         pass
@@ -28,6 +47,7 @@ def main(args=None):
 
     node.destroy_node()
     rclpy.shutdown()
+
 
 if __name__ == '__main__':
     main()
