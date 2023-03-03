042dca7d19a13f4c5c8449e62929f2c7c17f3a26
Adam Keating <adamkeating@xtra.co.nz>
2023-03-03 13:43:18 +1300

    changed licence, added callback functions where required, added successful msg test file

--- src/moa/moa_controllers/moa_controllers/ack_to_motec.py
+++ src/moa/moa_controllers/moa_controllers/ack_to_motec.py
@@ -1,13 +1,16 @@
 #/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
+from ctypes import c_uint8
+import time
 
 from ackermann_msgs import AckermannDriveStamped
-from diagnostic_msgs import DiagnosticStatus
+from diagnostic_msgs import DiagnosticStatus #maybe change to DiagnosticArray
+from moa_msgs import CANSendReq
 
 class ack_to_mot(Node):
     def __init__(self):
-        super().__init__('ackermann_to_motec') #node name
+        super().__init__('ackermann_to_motec') #node name   
 
         #create subscriber for ackermann input
         self.subscription = self.create_subscription(
@@ -24,20 +27,32 @@ class ack_to_mot(Node):
             10                         #qos
         )
         
-        # #create client for heartbeat server
-        # self.heartbeat = self.create_client(
-            
-        # )
+        #create client for heartbeat server
+        self.heartbeat = self.create_client( #Unsure if needs to be server or client
+            heartbeat_type,
+            'heartbeat'
+        )
 
-        # #create server for CAN
-        # self.server = self.create_service(
+        #create server for CAN
+        self.server = self.create_service( #Unsure if needs to be server or client
+            CANSendReq,
+            'CAN_SEND_SRV',
+            self.can_srv_callback
+        )
 
-        # )
 
-
-    def listener_callback(self):
+    def listener_callback(self, msg): #TODO
+        input = msg.data
+        pass
+        
+    def send_beat(): #TODO might need to be async process
         pass
 
+    def can_srv_callback(self, request, response): #TODO 
+        if request is not None:
+            response = True
+        return response
+        
 
 def main(args=None):
     rclpy.init(args=args)
--- src/moa/moa_controllers/setup.py
+++ src/moa/moa_controllers/setup.py
@@ -16,7 +16,7 @@ setup(
     maintainer='Adam Keating',
     maintainer_email='adam.keating@fsae.co.nz',
     description='Converts stamped ackermann msg to CAN msg for motec to read',
-    license='TODO: License declaration',
+    license='MIT',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
