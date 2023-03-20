ee54bf1bf60d22b2136a18eb56ebc1733bb142f1
Adam Keating <adam.keating@fsae.co.nz>
2023-03-20 23:42:32 +1300

    WIP

--- src/moa/moa_controllers/test/test_ackermann.py
+++ src/moa/moa_controllers/test/test_ackermann.py
@@ -1,1 +1,15 @@
 #/usr/bin/env python3
+import pytest
+import rclpy
+
+@pytest.fixture(scope='module')
+def ros_node():
+    rclpy.init()
+    node = rclpy.create_node('test_node')
+    yield node
+    node.destroy_node()
+    rclpy.shutdown()
+
+def test_node_functionality(ros_node):
+    # test the functionality of the ROS2 node
+    assert 
