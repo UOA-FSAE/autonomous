7b889d7dc1f92b29f7b476da6eaaad67dff0c401
Adam Keating <adam.keating@fsae.co.nz>
2023-03-27 20:56:44 +1300

    Commented out compression function/associated libraries and double checked tests

--- src/moa/moa_controllers/moa_controllers/ack_to_can.py
+++ src/moa/moa_controllers/moa_controllers/ack_to_can.py
@@ -11,60 +11,18 @@ from std_msgs.msg import Header
 from builtin_interfaces.msg import Time
 
 import numpy as np
-import zlib
+# import zlib
 from math import pi
 
 
-def compress_floats(values:np.ndarray) -> bytes:    
-    # Compute the differences between consecutive values
-    differences = np.diff(values)
+# def compress_floats(values:np.ndarray) -> bytes:    
+#     # Compute the differences between consecutive values
+#     differences = np.diff(values)
 
-    # Compress the differences using zlib
-    compressed = zlib.compress(differences, level=9)
+#     # Compress the differences using zlib
+#     compressed = zlib.compress(differences, level=9)
 
-    return compressed
-
-
-def ackermann_to_can_parser(node: Node, ack_msg: AckermannDriveStamped) -> Optional[CANStamped]: #type for option optionalnull
-        #checks before sending Ackermann
-        if 0 < {ack_msg.drive.speed} > 120/3.6: #m/s   
-            node.get_logger().warn(f'ackermann drive SPEED out of bounds')
-            return None
-        
-        elif 0 < {ack_msg.drive.acceleration} > 256: #m/s^2
-            node.get_logger().warn(f'ackermann drive ACCLERATION out of bounds')
-            return None
-
-        elif 0 < {ack_msg.drive.jerk}: #m/s^3 
-            #not too fussed about assign 1 byte (minifloat)
-            node.get_logger().warn(f'ackermann drive JERK out of bounds')
-            return None
-        
-        elif -45*pi/180 < {ack_msg.drive.steering_angle} > 45*pi/180: #radians
-            node.get_logger().warn(f'ackermann drive STEERING_ANGLE out of bounds')
-            return None
-        
-        elif 0 < {ack_msg.drive.steering_angle_velocity} < 1: #radians/s
-            #unsure of upper limit def dont need more than 1
-            node.get_logger().warn(f'ackermann drive STEERING_ANGLE_VELOCITY out of bounds')
-            return None
-        
-        #format values of Ackermann
-        ackermann_vals = [
-            np.uint8(round(ack_msg.drive.speed)), #(functionally limited to 100-120km/h) integer and round 
-            np.uint8(round(ack_msg.drive.acceleration)), #need to check between 0-256 
-            np.uint8(ack_msg.drive.jerk*1000), #need to check between 0-256 if dont need document that dont need and don't parse
-            np.float16(ack_msg.drive.steering_angle), #can be negative (assume between +-45 degrees change to radians) make float16
-            np.uint8(ack_msg.drive.steering_angle_velocity*1000), #can be negative between 0-1 (take 1 byte 8 bit minifloat)
-            ]
-
-        # #compress Ackermann values
-        # compressed_ack_vals = compress_floats(ackermann_vals)
-        
-        # #separate compressed ackermann values to list of bytes for CAN
-        # data_packets = [compressed_ack_vals[i:i+1] for i in range(0, len(compressed_ack_vals), 1)]
-
-        return ackermann_vals
+#     return compressed
 
 
 class ack_to_can(Node):
@@ -86,6 +44,48 @@ class ack_to_can(Node):
             10  
         )
 
+    def ackermann_to_can_parser(self, ack_msg: AckermannDriveStamped) -> Optional[CANStamped]: #type for option optionalnull
+        #checks before sending Ackermann
+        if 0 < {ack_msg.drive.speed} > 120/3.6: #m/s   
+            self.get_logger().warn(f'ackermann drive SPEED out of bounds')
+            return None
+        
+        elif 0 < {ack_msg.drive.acceleration} > 256: #m/s^2
+            self.get_logger().warn(f'ackermann drive ACCLERATION out of bounds')
+            return None
+
+        elif 0 < {ack_msg.drive.jerk}: #m/s^3 
+            #not too fussed about assign 1 byte (minifloat)
+            self.get_logger().warn(f'ackermann drive JERK out of bounds')
+            return None
+        
+        elif -45*pi/180 < {ack_msg.drive.steering_angle} > 45*pi/180: #radians
+            self.get_logger().warn(f'ackermann drive STEERING_ANGLE out of bounds')
+            return None
+        
+        elif 0 < {ack_msg.drive.steering_angle_velocity} < 1: #radians/s
+            #unsure of upper limit def dont need more than 1
+            self.get_logger().warn(f'ackermann drive STEERING_ANGLE_VELOCITY out of bounds')
+            return None
+        
+        #format values of Ackermann
+        ackermann_vals = [
+            np.uint8(round(ack_msg.drive.speed)), #(functionally limited to 100-120km/h) integer and round 
+            np.uint8(round(ack_msg.drive.acceleration)), #need to check between 0-256 
+            np.uint8(ack_msg.drive.jerk*1000), #need to check between 0-256 if dont need document that dont need and don't parse
+            np.float16(ack_msg.drive.steering_angle), #can be negative (assume between +-45 degrees change to radians) make float16
+            np.uint8(ack_msg.drive.steering_angle_velocity*1000), #can be negative between 0-1 (take 1 byte 8 bit minifloat)
+            ]
+
+        # #compress Ackermann values
+        # compressed_ack_vals = compress_floats(ackermann_vals)
+        
+        # #separate compressed ackermann values to list of bytes for CAN
+        # data_packets = [compressed_ack_vals[i:i+1] for i in range(0, len(compressed_ack_vals), 1)]
+
+        return ackermann_vals
+
+
     def ack_to_can_publish_callback(self, ack_msg: AckermannDriveStamped):
         can_msg = CANStamped 
 
@@ -97,14 +97,15 @@ class ack_to_can(Node):
         #set CAN header/data/id 
         can_msg.header = can_header
         can_msg.can.id = 56 #TODO need to change to dynamic
-        can_msg.can.data = ackermann_to_can_parser(ack_msg)
+        can_msg.can.data = self.ackermann_to_can_parser(ack_msg)
 
         if can_msg.can.data is not None:
             #publish CAN to topic
             self.can_pub.publish(can_msg)
         
-        return
+        return 
     
+
 def main(args=None):
     rclpy.init(args=args)
 
--- src/moa/moa_controllers/test/test_ackermann.py
+++ src/moa/moa_controllers/test/test_ackermann.py
@@ -2,7 +2,7 @@ import pytest
 import numpy as np
 from ackermann_msgs.msg import AckermannDrive, AckermannDriveStamped
 from moa_msgs.msg import CANStamped
-from ackermann_to_can import compress_floats, ackermann_to_can_parser, ack_to_can
+from moa_controllers.ack_to_can import ack_to_can
 from unittest.mock import Mock
 
 
@@ -10,39 +10,43 @@ from unittest.mock import Mock
 def node():
     return ack_to_can()
 
-def test_compress_floats():
-    values = np.array([1.0, 1.5, 2.0, 2.5])
-    expected_result = b'x\x9c+\x01\x00\xf6\xff,\x02\x00\xfa\xff'
-    assert compress_floats(values) == expected_result
+# def test_compress_floats():
+#     values = np.array([1.0, 1.5, 2.0, 2.5])
+#     expected_result = b'x\x9c+\x01\x00\xf6\xff,\x02\x00\xfa\xff'
+#     assert compress_floats(values) == expected_result
 
-def test_ackermann_to_can_parser_out_of_bounds(node):
+
+def test_ackermann_to_can_parser_out_of_bounds(node: ack_to_can):
     ack_msg = AckermannDriveStamped()
+    
     ack_msg.drive.speed = 130/3.6
-    assert ackermann_to_can_parser(node, ack_msg) is None
+    assert node.ackermann_to_can_parser(ack_msg) is None
 
     ack_msg.drive.speed = 0
     ack_msg.drive.acceleration = 257
-    assert ackermann_to_can_parser(node, ack_msg) is None
+    assert node.ackermann_to_can_parser(ack_msg) is None
 
     ack_msg.drive.acceleration = 0
     ack_msg.drive.jerk = -1
-    assert ackermann_to_can_parser(node, ack_msg) is None
+    assert node.ackermann_to_can_parser(ack_msg) is None
 
     ack_msg.drive.jerk = 0
     ack_msg.drive.steering_angle = 50*np.pi/180
-    assert ackermann_to_can_parser(node, ack_msg) is None
+    assert node.ackermann_to_can_parser(ack_msg) is None
 
     ack_msg.drive.steering_angle = 0
     ack_msg.drive.steering_angle_velocity = -1.5
-    assert ackermann_to_can_parser(node, ack_msg) is None
+    assert node.ackermann_to_can_parser(ack_msg) is None
 
-def test_ackermann_to_can_parser_valid_values(node):
+
+def test_ackermann_to_can_parser_valid_values(node: ack_to_can):
     ack_msg = AckermannDriveStamped()
     ack_msg.drive.speed = 20/3.6 # m/s
     ack_msg.drive.acceleration = 128 # m/s^2
     ack_msg.drive.jerk = 0.001 # m/s^3
     ack_msg.drive.steering_angle = np.pi/4 # radians
     ack_msg.drive.steering_angle_velocity = 0.5 # rad/s
+    
     expected_result = [
         np.uint8(round(ack_msg.drive.speed)),
         np.uint8(round(ack_msg.drive.acceleration)),
@@ -50,10 +54,12 @@ def test_ackermann_to_can_parser_valid_values(node):
         np.float16(ack_msg.drive.steering_angle),
         np.uint8(ack_msg.drive.steering_angle_velocity*1000),
     ]
-    can_msg = ack_to_can().ackermann_to_can_parser(node, ack_msg)
-    assert can_msg == expected_result
+    
+    can_data = node.ackermann_to_can_parser(ack_msg) 
+    assert can_data == expected_result
 
-def test_ack_to_can_publish_callback(node):
+
+def test_ack_to_can_publish_callback(node: ack_to_can):
     # Create a mock CAN publisher and inject it into the node
     mock_publisher = Mock()
     node.can_pub = mock_publisher
