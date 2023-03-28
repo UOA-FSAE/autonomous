aff847ed0c43abdea92bf3c1ce7085dacd5951de
Adam Keating <adam.keating@fsae.co.nz>
2023-03-28 16:48:34 +1300

    WIP: Device Transfer

--- src/moa/moa_controllers/moa_controllers/ack_to_can.py
+++ src/moa/moa_controllers/moa_controllers/ack_to_can.py
@@ -1,18 +1,17 @@
-#/usr/bin/env python3
+#!/usr/bin/env python3
+# Python imports
 from typing import Optional
 import rclpy
 from rclpy.node import Node
-import time
-
-from ackermann_msgs.msg import AckermannDriveStamped
-from ackermann_msgs.msg import AckermannDrive
-from moa_msgs.msg import CANStamped
-from std_msgs.msg import Header
-from builtin_interfaces.msg import Time
-
 import numpy as np
 # import zlib
 from math import pi
+
+# Ros Imports
+from ackermann_msgs.msg import AckermannDriveStamped
+from moa_msgs.msg import CANStamped
+from std_msgs.msg import Header
+from builtin_interfaces.msg import Time
 
 
 # def compress_floats(values:np.ndarray) -> bytes:    
@@ -27,65 +26,94 @@ from math import pi
 
 class ack_to_can(Node):
     def __init__(self):
-        super().__init__('ackermann_to_can') #node name (NB: MoTec listens to this)
+        super().__init__('ackermann_to_can') # node name (NB: MoTec listens to this)
 
-        #create subscriber for ackermann input
+        # create subscriber for ackermann input
         self.subscription = self.create_subscription(
-            AckermannDriveStamped,     #msg type
-            'cmd_val',                 #topic receiving from
-            self.listener_callback,    #callback function
-            10                         #qos profile
+            AckermannDriveStamped,     # msg type
+            'cmd_val',                 # topic receiving from
+            self.ack_to_can_publish_callback,    #callback function
+            10                         # qos profile
         )
 
-        #create server for CAN
-        self.can_pub = self.create_publisher( #Unsure if needs to be server or client
+        # create publisher for CAN
+        self.can_pub = self.create_publisher( 
             CANStamped,
             'pub_raw_can',
             10  
         )
 
-    def ackermann_to_can_parser(self, ack_msg: AckermannDriveStamped) -> Optional[CANStamped]: #type for option optionalnull
-        #checks before sending Ackermann
-        if 0 < ack_msg.drive.speed > 120/3.6: #m/s   
-            self.get_logger().warn(f'ackermann drive SPEED out of bounds')
-            return None
-        
-        elif 0 < ack_msg.drive.acceleration > 256: #m/s^2
-            self.get_logger().warn(f'ackermann drive ACCLERATION out of bounds')
+    def ackermann_to_can_parser(self, ack_msg: AckermannDriveStamped) -> Optional[CANStamped]:
+        """
+        Parses an AckermannDriveStamped message into a list of CAN data bytes.
+
+        Args
+        ---------------------------
+        - ack_msg: A ROS message of type `AckermannDriveStamped`, containing the Ackermann drive commands to be parsed.
+
+        Returns
+        ---------------------------
+        - A list of integers representing the parsed Ackermann drive values, suitable for sending over the CAN bus.
+        Returns `None` if any of the Ackermann values are out of bounds or invalid.
+
+        The following Ackermann drive values are checked for validity:
+        - `speed`: Must be between 0 and 120 km/h.
+        - `acceleration`: Must be between 0 and 256 m/s^2.
+        - `jerk`: Must be between 0 and 1 m/s^3.
+        - `steering_angle`: Must be between -45 and 45 degrees.
+        - `steering_angle_velocity`: Must be between 0 and 1 radians/s.
+
+        The following values are rounded to the nearest integer:
+        - `speed`
+        - `acceleration`
+        - `jerk` (multiplied by 1000 for precision)
+        - `steering_angle_velocity` (multiplied by 1000 for precision)
+
+        The `steering_angle` value is converted to a 2-byte representation for transmission over the CAN bus.
+        """
+
+        # checks before sending Ackermann
+        if 0 > ack_msg.drive.speed > 120/3.6:  # m/s   
+            self.get_logger().warn('ackermann drive SPEED out of bounds')
             return None
 
-        elif 0 < ack_msg.drive.jerk: #m/s^3 
-            #not too fussed about assign 1 byte (minifloat)
-            self.get_logger().warn(f'ackermann drive JERK out of bounds')
+        elif 0 > ack_msg.drive.acceleration > 255:  # m/s^2
+            self.get_logger().warn('ackermann drive ACCLERATION out of bounds')
             return None
-        
-        elif -45*pi/180 < ack_msg.drive.steering_angle > 45*pi/180: #radians
-            self.get_logger().warn(f'ackermann drive STEERING_ANGLE out of bounds')
-            return None
-        
-        elif 0 < ack_msg.drive.steering_angle_velocity < 1: #radians/s
-            #unsure of upper limit def dont need more than 1
-            self.get_logger().warn(f'ackermann drive STEERING_ANGLE_VELOCITY out of bounds')
-            return None
-        
-        #format values of Ackermann
-        speed = np.uint8(round(ack_msg.drive.speed)) #(functionally limited to 100-120km/h) integer and round 
-        acceleration = np.uint8(round(ack_msg.drive.acceleration))
-        jerk = np.uint8(ack_msg.drive.jerk*1000)
-        steering_angle = np.float16(ack_msg.drive.steering_angle)
-        steering_angle_vel = np.uint8(ack_msg.drive.steering_angle_velocity*1000)
-        
-        #separator for steering_angle
-        sav_size = len(steering_angle_vel)
 
-        #Compose CAN data packet
+        elif 0 > ack_msg.drive.jerk > 1:  # m/s^3 
+            # unsure of upper limit
+            # not too fussed about assign 1 byte 
+            self.get_logger().warn('ackermann drive JERK out of bounds')
+            return None
+        
+        elif -45*pi/180 > ack_msg.drive.steering_angle > 45*pi/180:  # radians
+            self.get_logger().warn('ackermann drive STEERING_ANGLE out of bounds')
+            return None
+
+        elif 0 > ack_msg.drive.steering_angle_velocity > 1:  # radians/s
+            # unsure of upper limit definitely dont need more than 1
+            self.get_logger().warn('ackermann drive STEERING_ANGLE_VELOCITY out of bounds')
+            return None
+
+        # format values of Ackermann
+        speed = round(ack_msg.drive.speed)
+        acceleration = round(ack_msg.drive.acceleration)
+        jerk = round(ack_msg.drive.jerk*1000)
+        steering_angle = np.float16(ack_msg.drive.steering_angle).tobytes()
+        steering_angle_vel = round(ack_msg.drive.steering_angle_velocity*1000)
+
+        # separator for steering_angle
+        s_a_size = len(steering_angle)
+
+        # Compose CAN data packet
         ackermann_vals = [
-            speed.tobytes(),
-            acceleration.tobytes(), 
-            jerk.tobytes(), 
-            steering_angle.tobytes()[:sav_size//2], 
-            steering_angle.tobytes()[sav_size//2:],
-            steering_angle_vel.tobytes(), 
+            speed,
+            acceleration,
+            jerk,
+            steering_angle[:s_a_size//2],
+            steering_angle[s_a_size//2:],
+            steering_angle_vel,
             ]
 
         # #compress Ackermann values
@@ -98,24 +126,22 @@ class ack_to_can(Node):
 
 
     def ack_to_can_publish_callback(self, ack_msg: AckermannDriveStamped):
-        can_msg = CANStamped 
+        can_msg = CANStamped()
 
-        #configure header
+        # configure header
         can_header = Header()
         can_header.stamp = self.get_clock().now()
         can_header.frame_id = 'ackermann_to_can'
 
-        #set CAN header/data/id 
+        # set CAN header/data/id 
         can_msg.header = can_header
         can_msg.can.id = 56 #TODO need to change to dynamic
         can_msg.can.data = self.ackermann_to_can_parser(ack_msg)
 
         if can_msg.can.data is not None:
-            #publish CAN to topic
+            # publish CAN to topic
             self.can_pub.publish(can_msg)
-        
-        return 
-    
+
 
 def main(args=None):
     rclpy.init(args=args)
