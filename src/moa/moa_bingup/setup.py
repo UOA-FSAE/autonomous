38a3c5ad75582b142cee303235d6c6475f639f4f
Jack17432 <chrisgraham908@gmail.com>
2023-04-25 18:01:05 +1200

    added join for launch files

--- src/moa/moa_bingup/setup.py
+++ src/moa/moa_bingup/setup.py
@@ -1,3 +1,5 @@
+import os
+from glob import glob
 from setuptools import setup
 
 package_name = 'moa_bingup'
@@ -10,6 +12,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('launch/*/*launch.[pxy][yma]*')),
+        (os.path.join('share', package_name), glob('launch/*launch.[pxy][yma]*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
