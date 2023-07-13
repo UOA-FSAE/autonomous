534fadb88c13237286ab7246c2104cb2734314f0
Tanish Bhatt <tbha814@aucklanduni.ac.nz>
2023-07-13 23:25:19 +1200

    Last commit - added joint state publisher

--- src/moa/moa_bringup/launch/urdf_model.launch.py
+++ src/moa/moa_bringup/launch/urdf_model.launch.py
@@ -14,13 +14,21 @@ def generate_launch_description():
     xacro_file = os.path.join(path,'urdf','moa_robot.urdf.xacro')
     # process xacro file
     robot_description = xacro.process_file(xacro_file)
-
+    
     # robot state publisher node
     robot_state_pub_node = Node(
             package='robot_state_publisher',
             executable='robot_state_publisher',
             output='screen',
-            parameters=[{'robot_description': robot_description.toxml()}]
+            parameters=[{'robot_description':robot_description.toxml()}]
+        )
+    
+    # joint state publisher node
+    joint_state_pub_node = Node(
+            package='joint_state_publisher',
+            executable='joint_state_publisher',
+            remappings=[('joint_states','/joint_states')],
+            parameters=[{'robot_description':str(robot_description.toxml())}]
         )
 
-    return LaunchDescription([robot_state_pub_node])
+    return LaunchDescription([robot_state_pub_node, joint_state_pub_node])
