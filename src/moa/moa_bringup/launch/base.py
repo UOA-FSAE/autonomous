b1285d4bc608de209d9110a2c0f4614299724d01
Adam Keating <107606965+csprono@users.noreply.github.com>
2023-07-18 22:36:22 +1200

    Revert "21 feat urdf"

--- src/moa/moa_bringup/launch/base.py
+++ src/moa/moa_bringup/launch/base.py
@@ -1,9 +1,5 @@
 import launch
 import launch_ros.actions
-from launch.actions import IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from ament_index_python import get_package_share_directory
-import os
 
 def generate_launch_description():  
     return launch.LaunchDescription([
@@ -17,11 +13,6 @@ def generate_launch_description():
         #     package='moa_driver',
         #     executable='can_interface_jnano',
         #     name='can_interface_jnano'),
-
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource([os.path.join(
-                get_package_share_directory('moa_description'), 'launch'),
-                            '/urdf_model.py'])),
 
         launch_ros.actions.Node(
             package='foxglove_bridge',
