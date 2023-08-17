6ee0856430cb28eb81649c8f575bf85d799fce9b
dyu056-fsae <daniel.yu@fsae.co.nz>
2023-08-17 22:44:52 +1200

    Little name changing

--- src/planning/occupancy_grid/occupancy_grid/occupancy_grid_service.py
+++ src/planning/occupancy_grid/occupancy_grid/occupancy_grid_service.py
@@ -6,16 +6,33 @@ from rclpy.node import Node
 
 class Occupancy_grid(Node):
 
+    #Example codes
     def __init__(self):
         super().__init__('Occupancy_grid')
         self.srv = self.create_service(AddTwoInts, 'add_two_ints', self.add_two_ints_callback)
 
+
+    
     def add_two_ints_callback(self, request, response):
         response.sum = request.a + request.b
         self.get_logger().info('Incoming request\na: %d b: %d' % (request.a, request.b))
 
         return response
+    
+    #Actual coding happening from here
+    ##############################Populating occupancy grid
+    def populating_occupancy_grid(self):
+        pass;
 
+    ##############################Boundary mapping
+
+
+    ##############################Bodyfit adjustment
+
+
+    ##############################Driveable region decision
+
+    
 
 def main():
     rclpy.init()
