71cd69fc0440a3f7521f5e6c33eb5de065f73393
Zane Larking <35547430+Zane-Larking@users.noreply.github.com>
2024-02-21 06:48:22 +1300

    Way points

--- src/moa/moa_controllers/moa_controllers/pure_pursuit.py
+++ src/moa/moa_controllers/moa_controllers/pure_pursuit.py
@@ -5,39 +5,69 @@ import rclpy
 from rclpy.node import Node
 import numpy as np
 from math import pi
-from geometry_msgs.msg import PoseArray
-from moa_msg.msg import ConeMap
+from math import sqrt
+
+from geometry_msgs.msg import PoseArray, Pose
+from moa_msg.msg import ConeMap, Cone
 from ackermann_msgs.msg import AckermannDrive
 
 
 
-class path_planning(Node):
+class PurePursuitController(Node):
     def __init__(self):
         super().__init__("Pure_Pursuit_Controller")
         self.get_logger().info("Path Planning Node Started")
         
         self.steering_angle = 0
         self.look_ahead = 100
-        self.pos = (0,0)
-
+        self.pos = None
+        self.way_point = None
         # subscribe to best trajectory
         self.best_trajectory_sub = self.create_subscription(PoseArray, "moa/selected_trajectory", self.selected_trajectory_handler, 5)
-        self.cone_map_sub = self.create_subscription(PoseArray, "moa/selected_trajectory", self.cone_map_handler, 5)
         self.cmd_vel_pub = self.create_publisher(AckermannDrive, "cmd/vel", 5)
-        
 
-    def cone_map_handler(msg: ConeMap):
-        self.car_pose = msg.cones[0].pose.position
+        self.timer = self.create_timer(5, self.publish_ackermann)
+
+    def selected_trajectory_handler(self, poseArray: PoseArray) -> None:
+        # The first marker location is the car 
+        # TODO separate into its own topic
+        self.car_pose = poseArray[0].position
         # The cars orientation is stored as radians from the car's initial
         # orientation (following right hand rule for positive and negative
         # direction) as the manitude of the quaternion (w). 
-        self.car_orientation = msg.cones[0].pose.orientation.w
+        self.car_orientation = poseArray[0].orientation.w
 
-    def selected_trajectory_handler():
+        cones = poseArray[1:]
+        self.way_point = self.way_point_look_ahead(cones)
+
+        
         self.set_steering_angle(self.curvator())
 
-    def publish_ackermann():
+        self.cmd_vel_pub.publish()
+
+    def extract_cones(self, coneMap: ConeMap) -> list[Cone]:
+        return coneMap[1:]
+    
+    def way_point_look_ahead(self, cones: ConeMap) -> Pose:
+        way_point = cones[0].pose
+        distance = float('inf')
+        for cone in cones:
+            distance_from_look_ahead_radius = self.distance_from_look_ahead_radius(cone.pose.position)
+            if distance_from_look_ahead_radius < distance:
+                way_point = cone.pose
+                distance = distance_from_look_ahead_radius
+        return way_point
+
+    def distance_from_look_ahead_radius(self, pos):
+        return abs(self.distanceTo(pos) - self.look_ahead)
+
+    def distanceTo(self, pos):
+        return sqrt(pow((self.pose.postion.x - pos.x),2) + pow((self.pose.postion.y - pos.y),2))
+
+    def publish_ackermann(self) -> None:
         
+        if (self.steering_angle == None): return
+
         args = {"steering_angle": self.steering_angle,
                 "steering_angle_velocity": 0.0,
                 "speed": self.current_speed,
@@ -49,12 +79,27 @@ class path_planning(Node):
     def lateral_distance()
         
 
-    def arc_radius():
+    def arc_radius(self):
         y = self.lateral_distance(self.way_point)
         pow(self.look_ahead, 2)  / 2 * y
 
-    def curvator():
-        self.look_ahead
+    def curvator(self):
+        return 1/self.arc_radius()
 
-    def set_steering_angle():
-        steering_angle = curvator
+    def set_steering_angle(self, angle):
+        self.steering_angle = angle
+
+
+
+def main():
+    rclpy.init()
+    node = PurePursuitController()
+    try:
+        rclpy.spin(node)
+    except Exception as e:
+        print(f"node spin error: {e}")
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == "__main__":
+    main()
