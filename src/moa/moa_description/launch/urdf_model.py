106d4a3d95ecd0b095074206be6e7ef1451ad3a7
Tanish Bhatt <Tanish.Bhatt@fsae.co.nz>
2024-03-13 18:53:44 +1300

    trying to setup gazebo and urdf

--- src/moa/moa_description/launch/urdf_model.py
+++ src/moa/moa_description/launch/urdf_model.py
@@ -1,13 +1,25 @@
 import os
 
-from ament_index_python import get_package_share_directory
+from ament_index_python import get_package_share_directory, get_package_prefix
 
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.actions import SetEnvironmentVariable
 
 import xacro
 
 def generate_launch_description():
+
+    pkg_install_path = get_package_share_directory('moa_description')
+
+    if 'GAZEBO_MODEL_PATH' in os.environ:
+        model_path =  os.environ['GAZEBO_MODEL_PATH'] + ':' + pkg_install_path
+    else:
+        model_path =  pkg_install_path
+    
+    gazebo_env = SetEnvironmentVariable("GAZEBO_MODEL_PATH", model_path)
 
     # get urdf file path
     path = os.path.join(get_package_share_directory('moa_description'))
@@ -20,15 +32,34 @@ def generate_launch_description():
             package='robot_state_publisher',
             executable='robot_state_publisher',
             output='screen',
-            parameters=[{'robot_description':robot_description.toxml()}]
+            parameters=[{'robot_description':robot_description.toxml(),
+                         'use_sim_time':True}]
         )
     
     # joint state publisher node
-    joint_state_pub_node = Node(
-            package='joint_state_publisher',
-            executable='joint_state_publisher',
-            remappings=[('joint_states','/joint_states')],
-            parameters=[{'robot_description':str(robot_description.toxml())}]
-        )
+    # joint_state_pub_node = Node(
+    #         package='joint_state_publisher',
+    #         executable='joint_state_publisher',
+    #         remappings=[('joint_states','/joint_states')],
+    #         parameters=[{'robot_description':str(robot_description.toxml())}]
+    #     )
 
-    return LaunchDescription([robot_state_pub_node, joint_state_pub_node])
+    # start gazebo
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('gazebo_ros'),'launch'), '/gazebo.launch.py']),
+        )
+    
+    # spawn entity
+    spawn_entity = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=['-topic','robot_description',
+                   '-entity','my_bot',
+                   '-x', '0',
+                   '-y', '0',
+                   '-z', '0'],
+        output='screen'
+    )
+
+    return LaunchDescription([robot_state_pub_node, gazebo, spawn_entity, gazebo_env])
