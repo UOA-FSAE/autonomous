ef0ff31135c55f9c775e29f558447a1ff5c2f74c
AdrianRosioru <adrianrosioru03@gmail.com>
2024-05-14 21:24:50 +1200

    We training

--- src/rl/rl_controller/rl_controller/controller.py
+++ src/rl/rl_controller/rl_controller/controller.py
@@ -7,6 +7,15 @@ import time
 import numpy as np
 from gymnasium import Env
 from gymnasium.spaces import Discrete, Dict, Box
+from gymnasium.vector.utils import create_empty_array
+from tensorflow import keras
+from keras.models import Sequential
+from keras.layers import Dense, Flatten
+from keras.optimizers.legacy import Adam
+from rl.agents.dqn import DQNAgent
+from rl.policy import BoltzmannQPolicy
+from rl.memory import SequentialMemory
+
 
 # Constants
 CAR_POSITION = Pose()
@@ -14,13 +23,13 @@ SELECTED_TRAJECTORY = None
 DESIRED_SPEED = 3.0
 TRACK_POINT_REACHED = 0
 TURNING_ANGLES = []
+TURNING_ANGLES_COUNT = 30
 
 ## RL Environment Node
 # This node is responsible for passing the parameters to the RL algorithm
 # and receiving the action from the RL algorithm to control the car
 # The parameters passed are the car position, selected trajectory, and desired speed
 # The action received is the desired speed
-
 class RLEnvironmentNode(Node):
     def __init__(self):
         super().__init__('rl_controller')
@@ -30,6 +39,7 @@ class RLEnvironmentNode(Node):
         self.car_position = CAR_POSITION
         self.selected_trajectory = SELECTED_TRAJECTORY
         self.desired_speed = DESIRED_SPEED
+        self.reset_in_progress = False
 
         self.track_point_reached = TRACK_POINT_REACHED
         self.turning_angles = TURNING_ANGLES
@@ -86,7 +96,7 @@ class RLEnvironmentNode(Node):
                 angle_offset = angle
             angle_list.append(abs(angle - angle_offset))
 
-        self.turning_angles = angle_list
+        self.turning_angles = np.asarray(angle_list)
 
 # Publish desired speed
     def publish_desired_speed(self, action):
@@ -106,10 +116,22 @@ class RLEnvironmentNode(Node):
 # Get observation
     def get_observation(self):
         # Return the current observation
+        # return first 30 angles and speed as list
+
+        if len(self.turning_angles) < TURNING_ANGLES_COUNT:
+            angles = np.zeros(TURNING_ANGLES_COUNT)
+            for i in range(len(self.turning_angles)):
+                angles[i] = self.turning_angles[i]
+            return {
+                'angles': angles[:TURNING_ANGLES_COUNT],
+                'speed': self.desired_speed
+            }
+        
         return {
-            'angles': self.turning_angles,
+            'angles': self.turning_angles[:TURNING_ANGLES_COUNT],
             'speed': self.desired_speed
         }
+        
 
 # Get reward
     def get_reward(self):
@@ -120,6 +142,10 @@ class RLEnvironmentNode(Node):
 # Reset environment
     def reset_environment(self):
         # Reset the environment to the initial state
+        if self.reset_in_progress:
+            return
+        
+        self.reset_in_progress = True
         print("TIMESTEPS REACHED")
 
         self.delete_car_pub.publish(String(data="test"))
@@ -131,6 +157,7 @@ class RLEnvironmentNode(Node):
         time.sleep(1)
         self.track_point_reached = 0
         self.publish_desired_speed(-1.0)
+        self.reset_in_progress = False
 
 def main(args=None):
     rclpy.init(args=args)
@@ -142,32 +169,61 @@ def main(args=None):
 
     env = CarEnv(rl_environment_node)
 
-    while (1):
-        time.sleep(0.1)
-        action = env.action_space.sample()
-        obs, reward, done, info = env.step(action)
+    obs = env.reset()
+    actions = env.action_space.n
+    time.sleep(1)
 
-        rl_environment_node.get_logger().info("Observation: " + str(obs))
+    # rl_environment_node.get_logger().info("Observation: " + str(obs))
 
-        if done:
-            obs = env.reset()
+    dqn = build_agent(build_model(obs, actions), actions)
+    dqn.compile(Adam(learning_rate=0.01), metrics=['mae'])
+    dqn.fit(env, nb_steps=500000, visualize=False, verbose=1)
+
+    dqn.save_weights('dqn_weights.h5f', overwrite=True)
 
     rl_environment_node.destroy_node()
     rclpy.shutdown()
 
+
 if __name__ == '__main__':
     main()
+
+
+def encode_obs(states) -> np.array:
+    angles = states['angles']
+    speed = states['speed']
+    speed_array = np.array([speed])  # Convert speed to a single-element array
+    array = np.concatenate([angles, speed_array], axis=0) # Concatenate along the first axis
+    return array
+
+
+
+def build_model(obs, actions):
+    model = Sequential()
+    model.add(Flatten(input_shape=(1,) + obs.shape))
+    model.add(Dense(64, activation='relu'))
+    model.add(Dense(32, activation='relu'))
+    model.add(Dense(actions, activation='linear'))
+    print(model.summary())
+    return model     
+    
+
+def build_agent(model, actions):
+    policy = BoltzmannQPolicy()
+    memory = SequentialMemory(limit=500000, window_length=1)
+    dqn = DQNAgent(model=model, memory=memory, policy=policy, nb_actions=actions, nb_steps_warmup=10, target_model_update=1e-2)
+    return dqn
+
 
 ## Car Environment class
 # This class defines the environment for the car
 # The action space is defined as Discrete with 3 actions
 # The observation space is defined as angles and speed
-
 class CarEnv(Env):
     def __init__(self, rl_environment_node):
         # Define action and observation space
         self.action_space = Discrete(3)
-        self.max_timesteps = 1000
+        self.max_timesteps = 5000
         self.timesteps = 0
         self.num_angles = 30
 
@@ -182,19 +238,34 @@ class CarEnv(Env):
         # Execute one time step within the environment
         self.timesteps += 1
         if self.timesteps >= self.max_timesteps:
-            return [], 0, True, {}
+            raw_observation = self.rl_environment_node.get_observation()
+            obs = create_empty_array(self.observation_space, n=1, fn=np.zeros)
+            obs['angles'] = raw_observation['angles'][:self.num_angles]
+            obs['speed'] = raw_observation['speed']
+            obs = encode_obs(obs)
+            return obs, 0, True, {}
         
         reward = self.rl_environment_node.get_reward()
 
-        observation = self.rl_environment_node.get_observation()
+        raw_observation = self.rl_environment_node.get_observation()
+        obs = create_empty_array(self.observation_space, n=1, fn=np.zeros)
+        obs['angles'] = raw_observation['angles'][:self.num_angles]
+        obs['speed'] = raw_observation['speed']
+        obs = encode_obs(obs)
 
         self.rl_environment_node.publish_desired_speed(action)
         
-        # return observation, reward, done, info
-        return np.array(self.rl_environment_node.get_observation()), reward, False, {}
+        return obs, reward, False, {}
         
     def reset(self):
         # Reset the state of the environment to an initial state
         self.rl_environment_node.reset_environment()
         self.timesteps = 0
-        return np.array(self.rl_environment_node.get_observation())
+
+        raw_observation = self.rl_environment_node.get_observation()
+        obs = create_empty_array(self.observation_space, n=1, fn=np.zeros)
+        obs['angles'] = raw_observation['angles'][:self.num_angles]
+        obs['speed'] = raw_observation['speed']
+        obs = encode_obs(obs)
+        return obs
+        
