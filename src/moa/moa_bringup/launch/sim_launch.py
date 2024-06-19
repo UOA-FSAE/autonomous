2a67baf0fff509414f9f400a71a205800bb58318
fsae computer <>
2024-06-19 14:06:00 +1200

    configuring controller to send ackerman for gokart

--- src/moa/moa_bringup/launch/sim_launch.py
+++ src/moa/moa_bringup/launch/sim_launch.py
@@ -4,11 +4,11 @@ import launch_ros.actions
 def generate_launch_description():
     return launch.LaunchDescription([
         # cone map
-        # launch_ros.actions.Node(
-        #     package='aruco_detection',
-        #     executable='aruco_detection',
-        #     name='aruco_detection'
-        # ),
+        launch_ros.actions.Node(
+            package='aruco_detection',
+            executable='aruco_detection',
+            name='aruco_detection'
+        ),
 
         # cone mapping
         launch_ros.actions.Node(
@@ -37,10 +37,17 @@ def generate_launch_description():
 
         # controller
         launch_ros.actions.Node(
-            package='moa_controllers',
-            executable='trajectory_follower',
-            name='trajectory_follower',
+            package='stanley_controller',
+            executable='controller',
+            name='controller',
         ),
+
+        # steer torque
+        # launch_ros.actions.Node(
+        #     package='steer_torque_from_ackermann',
+        #     executable='steer_torque_from_ackermann',
+        #     name='steer_torque_from_ackermann',
+        # ),
 
         # path viz
         launch_ros.actions.Node(
--- src/moa/moa_controllers/moa_controllers/ack_to_can.py
+++ src/moa/moa_controllers/moa_controllers/ack_to_can.py
@@ -83,7 +83,7 @@ class ack_to_can(Node):
             self.get_logger().warn('ackermann drive JERK out of bounds')
             return None
         
-        elif -45 > ack_msg.drive.steering_angle or ack_msg.drive.steering_angle > 45:  # radians
+        elif -22 > ack_msg.drive.steering_angle or ack_msg.drive.steering_angle > 22:  # degrees
             self.get_logger().warn('ackermann drive STEERING_ANGLE out of bounds')
             return None
 
