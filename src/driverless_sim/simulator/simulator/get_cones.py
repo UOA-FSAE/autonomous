d1f3dcf98db71959a3bba31f0c528df404cf2ac6
Tanish Bhatt <Tanish.Bhatt@fsae.co.nz>
2024-08-29 22:34:04 +1200

    get_cones node publishes cone map

--- src/driverless_sim/simulator/simulator/get_cones.py
+++ src/driverless_sim/simulator/simulator/get_cones.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-
 import rclpy
 from rclpy.node import Node
+
+from geometry_msgs.msg import Point
+from moa_msgs.msg import ConeMap
+
 import os
 import sys
 import matplotlib.pyplot as plt
+
 ## adds the fsds package located the parent directory to the pyhthon path
 path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../../..','Formula-Student-Driverless-Simulator','python'))
 sys.path.insert(0, path)
@@ -16,26 +20,39 @@ class get_cones(Node):
     def __init__(self):
         super().__init__("get_cones")
 
+        self.plot = False
         # connect to the simulator 
         self.client = fsds.FSDSClient(ip=os.environ['WSL_HOST_IP'])
         # Check network connection, exit if not connected
         self.client.confirmConnection()
         # After enabling setting trajectory setpoints via the api. 
         self.client.enableApiControl(True)
+        # create publisher
+        self.sim_cone_pub = self.create_publisher(ConeMap, "cone_map", 10)
 
-        self.count = 0
         self.get_cones_from_simulator()
 
+
     def get_cones_from_simulator(self):
-        lb, rb = self.find_cones()
+        lb, rb = self.find_cones()  # detect cones in simulation
 
-        plt.plot([P[0] for P in lb], [P[1] for P in lb], "*b")
-        plt.plot([P[0] for P in rb], [P[1] for P in rb], "*y")
-        plt.show()
+        msg = self.get_cone_map_msg(lb.copy(),rb.copy())   # convert the boundaries to point list
 
+        self.sim_cone_pub.publish(msg)  # publish
+
+        if self.plot:
+            plt.plot([P[0] for P in lb], [P[1] for P in lb], "*b", label="left")
+            plt.plot([P[0] for P in rb], [P[1] for P in rb], "*y", label="right")
+            for i in range(len(lb)):
+                plt.annotate(f"{i}",(lb[i][0], lb[i][1]))
+            for i in range(len(rb)):
+                plt.annotate(f"{i}",(rb[i][0], rb[i][1]))
+            plt.legend()
+            plt.show()
     
     def find_cones(self):
-        """ gets the exact position of the cones using the API - cheating as not sensor information used"""
+        """ Detects cones in the simulator
+        HOWEVER at the moment the exact positions of the cones is requested from API (change asap)"""
         # referee state
         ref_state = self.client.getRefereeState()
 
@@ -48,13 +65,24 @@ class get_cones(Node):
             x = cone_dict['x']
             y = cone_dict['y']
 
-            if color == 0:
-                leftboundary.append([x,y])
-            elif color == 1:
-                rightboundary.append([x,y])
+            if color == 0:  # type 0 color cone is blue
+                leftboundary.append([x,y,0.0])
+            elif color == 1:    # type 1 color cone is yellow
+                rightboundary.append([x,y,0.0])
 
         return leftboundary, rightboundary
 
+    def get_cone_map_msg(self, lb, rb):
+        try:
+            assert len(lb) == len(rb)
+        except Exception as e:
+            self.get_logger().error(e)
+        finally:
+            for i in range(len(lb)):
+                lb[i] = Point(x=lb[i][0],y=lb[i][1],z=lb[i][2])
+                rb[i] = Point(x=rb[i][0],y=rb[i][1],z=rb[i][2])
+
+        return ConeMap(left_cones=lb, right_cones=rb)
 
 def main(args=None):
     rclpy.init(args=args)
