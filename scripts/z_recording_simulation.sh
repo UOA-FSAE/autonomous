3811eca66d1d25d8287f8facc292435bc1ce58ac
AdrianRosioru <adrianrosioru03@gmail.com>
2024-09-22 17:39:22 +1200

    Rename some things

---
+++ scripts/z_recording_simulation.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+gnome-terminal -- bash -c "source run_cone_map.sh; exec bash"
+gnome-terminal -- bash -c "source run_cone_map_visualizer.sh; exec bash"
+gnome-terminal -- bash -c "source run_controller_visualizer.sh; exec bash"
+gnome-terminal -- bash -c "source run_controller.sh; exec bash"
+gnome-terminal -- bash -c "source run_foxglove_ros.sh; exec bash"
+gnome-terminal -- bash -c "source run_localization.sh; exec bash"
+gnome-terminal -- bash -c "source run_path_planning_visualizer.sh; exec bash"
+gnome-terminal -- bash -c "source run_path_planning.sh; exec bash"
+gnome-terminal -- bash -c "source run_simulation_car_control.sh; exec bash"
+
