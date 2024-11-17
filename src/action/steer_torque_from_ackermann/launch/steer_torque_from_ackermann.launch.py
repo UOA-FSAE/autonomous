13c88dd9e807a12772059bbe28db71cf3dd0698a
Jonty-Clark <jontyclark2005@gmail.com>
2024-11-17 14:55:47 +1300

    steer_torque_from_ackermann launch file

---
+++ src/action/steer_torque_from_ackermann/launch/steer_torque_from_ackermann.launch.py
@@ -0,0 +1,12 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='steer_torque_from_ackermann',
+            # namespace='', (not sure)
+            executable='converter',
+            name='steer_torque_converter'
+        )
+    ])
