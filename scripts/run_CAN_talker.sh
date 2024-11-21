7396cbde16446c09a55e7d570cafb72dd986660b
Waldo Singleton <92557683+waldosingleton@users.noreply.github.com>
2024-11-21 19:38:50 +1300

    Update CanTalk to cantalk

--- scripts/run_CAN_talker.sh
+++ scripts/run_CAN_talker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source install/setup.bash
-colcon build --packages-select CanTalk && ros2 run CanTalk candapter_node
+colcon build --packages-select CanTalk && ros2 run cantalk candapter_node
