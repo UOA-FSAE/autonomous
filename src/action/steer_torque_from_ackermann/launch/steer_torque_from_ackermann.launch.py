d23c3bb1e4a7db439bda9fd506033ddee9c433c5
Jonty-Clark <jontyclark2005@gmail.com>
2024-11-24 16:55:07 +1300

    Error fixed

--- src/action/steer_torque_from_ackermann/launch/steer_torque_from_ackermann.launch.py
+++ src/action/steer_torque_from_ackermann/launch/steer_torque_from_ackermann.launch.py
@@ -5,7 +5,7 @@ def generate_launch_description():
     return LaunchDescription([
         Node(
             package='steer_torque_from_ackermann',
-            executable='converter',
+            executable='steer_torque_from_ackermann',
             name='steer_torque_converter'
         )
     ])
