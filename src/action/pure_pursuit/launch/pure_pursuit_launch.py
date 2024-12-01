daca9d26dcea0594deede754a791e826a9f0b5a2
winola-whu <winolahu@gmail.com>
2024-12-01 15:31:47 +1300

    Added logs for when visualisation is enabled, made sure all of pure_pursuit visualisation functions are in the pure_pursuit node

--- src/action/pure_pursuit/launch/pure_pursuit_launch.py
+++ src/action/pure_pursuit/launch/pure_pursuit_launch.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
-# no parameters, run ros2 launch pure_pursuit pure_pursuit_launch.py 
+# to launch
+# ros2 launch pure_pursuit pure_pursuit_launch.py 
 
 
 from launch import LaunchDescription
