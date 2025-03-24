975ffd037d5d16c2034a510a0d645931dd93ca25
winola <winolahu@gmail.com>
2025-03-24 21:44:00 +1300

    zed launch shell script

--- scripts/run_zed_launch.sh
+++ scripts/run_zed_launch.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 source install/setup.bash
-colcon build --packages-select zed_launch && ros2 run zed_launch zed_launch_node
+#colcon build --packages-select zed_launch && ros2 run zed_launch zed_launch_node 
+## with sim
+colcon build --packages-select zed_launch && ros2 run zed_launch zed_launch_node recording1.svo2
