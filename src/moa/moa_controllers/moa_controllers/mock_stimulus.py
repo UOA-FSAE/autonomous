bf673c6588fdc811f657d64359ee7cdbc346b4ea
winola <winolahu@gmail.com>
2025-06-23 21:17:36 +1200

    Reverted Mock Stimulus shutdown guard condition

--- src/moa/moa_controllers/moa_controllers/mock_stimulus.py
+++ src/moa/moa_controllers/moa_controllers/mock_stimulus.py
@@ -10,15 +10,6 @@ class mock_stimulus_node(Node):
     def __init__(self, *vargs, **kwargs):
         super().__init__("stimulus_node", *vargs, **kwargs)
         
-        # Create a separate callback group for shutdown
-        self.shutdown_callback_group = ReentrantCallbackGroup()
-        
-        # Create guard condition with callback
-        self.shutdown_guard = GuardCondition(
-            self.on_shutdown_triggered,
-            callback_group=self.shutdown_callback_group,
-            context=self.context
-        )
         # real angle is 1.395 the magnitude of the input angle 
         self.declare_parameters(
             namespace='',
@@ -36,9 +27,6 @@ class mock_stimulus_node(Node):
         
         self.cmd_vel_pub = self.create_publisher(AckermannDriveStamped, "cmd_vel", 10)
       
-    def on_shutdown_triggered(self):
-        self.get_logger().log("shutting down")
-        
     def update_params(self):
         self.vel = self.get_parameter("vel").get_parameter_value().double_value
         self.accel = self.get_parameter("accel").get_parameter_value().double_value
@@ -70,30 +58,34 @@ class mock_stimulus_node(Node):
 
 
 from rclpy.context import Context
-from rclpy.executors import SingleThreadedExecutor, MultiThreadedExecutor
-from rclpy.guard_condition import GuardCondition
+from rclpy.executors import SingleThreadedExecutor
 import signal
 
+shutdown_requested = False
 
 def main(args=None):
     ctx = Context()
     
+    # Handle shutdown via Ctrl+C
     def sigint_handler(signum, frame):
+        global shutdown_requested
         node.get_logger().info("handling SIGINT - triggering shut down")
-        node.shutdown_guard.trigger()
-    
+        shutdown_requested = True
     signal.signal(signal.SIGINT, sigint_handler)
     rclpy.init(args=args, context=ctx, signal_handler_options=rclpy.signals.SignalHandlerOptions.NO)
-    executor = MultiThreadedExecutor(context=ctx, num_threads=2)
-    # executor = SingleThreadedExecutor(context=ctx)
+    
+    executor = SingleThreadedExecutor(context=ctx)
     
     node = mock_stimulus_node(context=ctx)
     executor.add_node(node)
     
-
+    
     try:
-        executor.spin() # shuts down internally on SIGINT signal
+        while ctx.ok() and not shutdown_requested:
+            executor.spin_once(timeout_sec=0)
     except KeyboardInterrupt:
+        # KeyboardInterrupts will not be raised on Ctrl+C as rcl does not handle the
+        # SIGINT signal
         node.get_logger().info("keyboard interrupt signal intercepted")
     finally:
         node.get_logger().info("shutting down")
