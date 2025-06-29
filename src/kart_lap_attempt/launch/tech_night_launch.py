f14042aec881a7096098842a6eab9563d6f05af4
Tofu <clob269@aucklanduni.ac.nz>
2025-06-29 14:33:46 +1200

    slight changes to bypass kalman filter for tech night

---
+++ src/kart_lap_attempt/launch/tech_night_launch.py
@@ -0,0 +1,47 @@
+import launch
+import launch.actions
+import launch_ros.actions
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            namespace='moa',
+            package='zed_launch',
+            executable='zed_launch_node',
+            name='perception'
+        ),
+        Node(
+            namespace='moa',
+            package='path_planning',
+            executable='fasttube_without_kalman',
+            name='centerline_planner'
+        ),
+        Node(
+            namespace='moa',
+            package='stanley_controller',
+            executable='controller',
+            name='stanley_controller'
+        ),
+        launch_ros.actions.Node(
+            namespace='moa',
+            package="CanTalk",
+            executable="candapter_node",
+            output="screen"
+        ),
+        launch_ros.actions.Node(
+            namespace='moa',
+            package="moa_controllers",
+            executable="ack_to_can_node",
+            name='ack_to_can',
+            output="screen"
+        ),
+        launch_ros.actions.Node(
+            namespace='moa',
+            package="path_planning_visualiser",
+            executable="image_throttler",
+            name='image_throttler',
+            output="screen"
+        )
+    ])
